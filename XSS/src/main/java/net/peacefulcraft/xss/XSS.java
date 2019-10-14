d8548bbb3906678a3315d08cbe279b5f612b4159
parsonswy <parsonswy@gmail.com>
2019-10-13 22:05:26 -0400

    Enbale TrenchBounties

---
+++ XSS/src/main/java/net/peacefulcraft/xss/XSS.java
@@ -0,0 +1,29 @@
+package net.peacefulcraft.xss;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class XSS extends JavaPlugin{
+
+	private static XSS instance = null;
+	public static XSS getInstance() { return instance; }
+	
+	private static XSSConfig config;
+	public static XSSConfig getXXSConfig() { return config; }
+	
+	private TrenchBounties tb = null;
+	
+	public void onEnable() {
+		instance = this;
+		saveDefaultConfig();
+		config = new XSSConfig(getConfig());
+		
+		tb = new TrenchBounties();
+		getServer().getPluginManager().registerEvents(tb, this);
+	}
+	
+	public void onDisable() {
+		tb.shutdown();
+		saveConfig();
+	}
+	
+}
